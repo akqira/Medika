eb9ddbe5fa677beb8a47b5e8962f7e5b3faa7eb8
Kaki <kader.kebir@gmail.com>
2026-06-05 21:58:05 +0200

    initial commit

---
+++ apps/backend/src/Medika.Api/Program.cs
@@ -0,0 +1,15 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddFastEndpoints()
+    .SwaggerDocument();
+
+var app = builder.Build();
+
+app.UseFastEndpoints()
+   .UseSwaggerGen();
+
+app.Run();
